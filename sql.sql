34eedeabe1bce58665da520b3c5de8263975266f
AndreVictor60 <andre.victor60@free.fr>
2017-11-22 08:42:39 +0100

    fini

---
+++ sql.sql
@@ -0,0 +1,12 @@
+Exercice 1:
+
+select * from frameworks where version like 'version 2%';
+
+Exercice 2:
+
+SELECT * FROM frameworks WHERE id=1 OR id=3;
+
+Exercice 3:
+
+select * from ide where date between '2010-01' and '2011-12-31';
+
